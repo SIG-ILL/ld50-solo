e1e38c07d11ea10052ba1f38c332aab88861db6e
B Bergfeld <b.bergfeld@hotmail.com>
2022-04-05 00:14:26 +0200

    Add check for WebGL to enable exit buttons.

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,7 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
+	[SerializeField]
+	private Button exitButton;
+
+	private void Awake() {
+		if(Application.platform == RuntimePlatform.WebGLPlayer) {
+			exitButton.gameObject.SetActive(false);
+		}
+	}
 	public void OnGameInfoButtonClicked() {
 		SceneManager.LoadScene("GameInfo");
 	}
